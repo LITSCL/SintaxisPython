b1f102e3a64f5fb55d97c740ff3d008228f23d2a
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-17 17:34:11 -0300

    Ok

--- pSistemaDeArchivos/FmoverUnArchivo.py
+++ pSistemaDeArchivos/FmoverUnArchivo.py
@@ -4,5 +4,5 @@ import shutil
 rutaOrigen: str = str(pathlib.Path().absolute()) + "/03-F.txt"
 rutaDestino: str = str(pathlib.Path().absolute()) + "/FmoverUnArchivo/03-F.txt"
 
-#Copiar y pegar archivo.
-shutil.move(rutaOrigen, rutaDestino)
+#Cortar y pegar archivo.
+shutil.move(rutaOrigen, rutaDestino) #La función "move", permite cortar un archivo y pegarlo en otra ubicación.
