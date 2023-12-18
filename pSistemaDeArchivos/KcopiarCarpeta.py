aa4eb263802bff7ec1953376ef087b19b2ff20d6
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-18 18:10:17 -0300

    Ok

--- pSistemaDeArchivos/KcopiarCarpeta.py
+++ pSistemaDeArchivos/KcopiarCarpeta.py
@@ -1,8 +1,8 @@
 import pathlib
 import shutil
 
-ruta_origen: str = str(pathlib.Path().absolute()) + "/mi_carpeta"
-ruta_destino: str = str(pathlib.Path().absolute()) + "/mi_carpeta_copia"
+ruta_origen: str = str(pathlib.Path().absolute()) + "/KcopiarCarpeta/mi_carpeta"
+ruta_destino: str = str(pathlib.Path().absolute()) + "/KcopiarCarpeta/mi_carpeta_copia"
 
 #Copiar y pegar una carpeta.
 shutil.copytree(ruta_origen, ruta_destino) #La función "copytree", permite copiar una carpeta y pegarla en otra ubicación.
