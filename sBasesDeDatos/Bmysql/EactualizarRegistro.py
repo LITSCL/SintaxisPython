c02c5ad90f4dc600132cf8e5f641750a0e4e59af
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-25 06:31:45 -0300

    Ok

--- sBasesDeDatos/Bmysql/EactualizarRegistro.py
+++ sBasesDeDatos/Bmysql/EactualizarRegistro.py
@@ -1,6 +1,6 @@
 import mysql.connector #Este módulo permite trabajar con el DBMS MySQL.
 
-#2. Conectarse a la base de datos (Los parámetros se deben entregar así omo variables iniciadas dentro, con los nombres en inglés).
+#1. Conectarse a la base de datos (Los parámetros se deben entregar así omo variables iniciadas dentro, con los nombres en inglés).
 conexion: object = mysql.connector.connect(
     host = "localhost",
     user = "root",
