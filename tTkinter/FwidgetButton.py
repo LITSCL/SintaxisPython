c9eafa345d1620d58a6b13617f78f5c775e4c18d
Daniel Álvarez <danielalvarez.litscl.melchiot@protonmail.com>
2023-12-29 10:05:32 -0300

    Ok

---
+++ tTkinter/FwidgetButton.py
@@ -0,0 +1,30 @@
+"""
+Button (Botón):
+Este widget permite a los usuarios realizar acciones al hacer click en el botón. Se puede asociar una función 
+o un comando al botón para que se ejecute cuando el usuario lo presiona.
+"""
+
+import pathlib
+from tkinter import *
+
+rutaAbsoluta: str = str(pathlib.Path().absolute())
+
+ventana: object = Tk()
+ventana.title("Ventana Principal")
+ventana.iconbitmap(rutaAbsoluta + "/recursos/icono.ico")
+ventana.geometry("750x450")
+ventana.resizable(1, 1)
+
+#1. Crear el Text, indicando a que ventana va a pertenercer.
+btn: object = Button(ventana)
+
+#2. Configurar el Button.
+btn.config(
+    text = "Texto del Botón",
+    cursor = "hand2"
+)
+
+#3. Empaquetar el Button (Esto permite que se muestre).
+btn.pack()
+
+ventana.mainloop()
---
+++ tTkinter/GwidgetCheckbutton.py
@@ -0,0 +1,50 @@
+"""
+Button (Botón):
+Este widget permite a los usuarios realizar acciones al hacer click en el botón. Se puede asociar una función 
+o un comando al botón para que se ejecute cuando el usuario lo presiona.
+"""
+
+import pathlib
+from tkinter import *
+
+rutaAbsoluta: str = str(pathlib.Path().absolute())
+
+ventana: object = Tk()
+ventana.title("Ventana Principal")
+ventana.iconbitmap(rutaAbsoluta + "/recursos/icono.ico")
+ventana.geometry("750x450")
+ventana.resizable(1, 1)
+
+#1. Crear los Checkbuttons, indicando a que ventana va a pertenercer.
+chk_1: object = Checkbutton(ventana)
+chk_2: object = Checkbutton(ventana)
+chk_3: object = Checkbutton(ventana)
+
+#2. Configurar los Checkbuttons.
+chk_1.config(
+    text = "Java",
+    cursor = "hand2",
+    onvalue = 1, #Valor que toma el Widget si está activado.
+    offvalue = 0 #Valor que toma el Widget si está desactivado.
+)
+
+chk_2.config(
+    text = "PHP",
+    cursor = "hand2",
+    onvalue = 1,
+    offvalue = 0
+)
+
+chk_3.config(
+    text = "C++",
+    cursor = "hand2",
+    onvalue = 1,
+    offvalue = 0,
+)
+
+#3. Empaquetar los Checkbuttons (Esto permite que se muestre).
+chk_1.pack()
+chk_2.pack()
+chk_3.pack()
+
+ventana.mainloop()
